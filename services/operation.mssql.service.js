2e9ab5bc280b2626bc4b279ae2a1a389f8b4e6ae
anRoswell <40361918+anRoswell@users.noreply.github.com>
2022-03-30 07:53:53 -0500

    se termina ruta initial parameters

--- services/operation.mssql.service.js
+++ services/operation.mssql.service.js
@@ -12,8 +12,10 @@ const log4js = require('../utils/log4js')()
 
 const initialParameters = async () => {
 	try {
-        const { recordset } = await Queries.querySP('[ope].[spOperation]')
-	    return recordset
+        const profesiones = await Queries.querySP('[csc].[spProfesiones]')
+        const empresas = await Queries.querySP('[csc].[spEmpresas]')
+        const sedes = await Queries.querySP('[csc].[spSedes]')
+	    return { profesiones: profesiones.recordsets, empresas: empresas.recordsets, sedes: sedes.recordsets }
     } catch (e) {
         log4js.error(`[action: operation metodo: initialParameters][msg: ${e.message}][file:${__filename}]`)
 		throw new Error(mensajes('DB_CONNECTION_ERROR').message)
